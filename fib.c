d3885777623e5b726ea6e080cde45e5340b07969
smminassian <sebastienminassian203@gmail.com>
2024-09-10 15:40:02 -0700

    So I know my code works
    But the issue is that scanf does not seem to work when its all in one line. I think its because I have two arrays and it gives me a seg fault cus I am scan fing the 20 and 2 spot of the arrays and there is nothing there.

--- fib.c
+++ fib.c
@@ -1,4 +1,75 @@
 
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// int fibbonachiRecurs(int num)
+// {
+//    if (num == 0 || num == 1)
+//    {
+//       return num;
+//    }
+//    else
+//    {
+//       return fibbonachiRecurs(num - 1) + fibbonachiRecurs(num - 2);
+//    }
+// }
+
+// int fibbonachiItera(int n)
+// {
+//    int num1 = 0;
+//    int num2 = 1;
+//    int num3;
+//    for (int i = 2; i <= n; i++)
+//    {
+//       if (n != 0)
+//       {
+//          num3 = num1 + num2;
+//          num1 = num2;
+//          num2 = num3;
+//       }
+//       else
+//       {
+//          return num1;
+//       }
+//    }
+//    return num3;
+// }
+
+// int main()
+// {
+//    int num1;
+//    char nameOfTextFile[20];
+//    char recursiveOrIterative[2];
+
+//    scanf("%d %s %s", &num1, recursiveOrIterative, nameOfTextFile);
+
+//    FILE *fpointer;
+//    int fileNum;
+
+//    fpointer = fopen(nameOfTextFile, "r");
+//    fscanf(fpointer, "%d", &fileNum);
+//    fclose(fpointer);
+
+//    int nthNum = num1 + fileNum;
+//    int FibbonachiNum;
+
+//    if (*recursiveOrIterative == 'i')
+//    {
+//       FibbonachiNum = fibbonachiItera(nthNum);
+//    }
+//    else if (*recursiveOrIterative == 'r')
+//    {
+//       FibbonachiNum = fibbonachiRecurs(nthNum);
+//    }
+
+//    printf("The %dth of the fibbonachi series is %d", nthNum, FibbonachiNum);
+
+//    return 0;
+// }
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -46,6 +117,10 @@ int main()
 
    FILE *fpointer;
    int fileNum;
+
+   fpointer = fopen(nameOfTextFile, "w");
+   fprintf(fpointer, "1");
+   fclose(fpointer);
 
    fpointer = fopen(nameOfTextFile, "r");
    fscanf(fpointer, "%d", &fileNum);
