e7a878831ca9b493c060b7a629b522f45ccb3822
smminassian <sebastienminassian203@gmail.com>
2024-09-14 16:13:22 -0700

    im trying to implement memoization.
    Here is the issue, I keep getting a malloc exception

--- fib.c
+++ fib.c
@@ -2,62 +2,72 @@
 #include <stdlib.h>
 #include <string.h>
 
-int fibbonachiRecurs(long long int num)
+int fibbonachiRecurs(int num, long long int* fibArray)
 {
+   int fibValue = 0;
    if (num == 0 || num == 1)
    {
       return num;
    }
-   else
+   else if (fibArray[num] == 0)
    {
-      return fibbonachiRecurs(num - 1) + fibbonachiRecurs(num - 2);
+      fibValue = fibbonachiRecurs(num - 1, fibArray) + fibbonachiRecurs(num - 2, fibArray);
+      fibArray[num] = fibValue;
    }
+  
+   return fibArray[num];
 }
 
 int fibbonachiItera(int n)
 {
-   long long int num1 = 0;
-   long long int num2 = 1;
-   long long int num3 = 0;
+   int num1 = 0;
+   int num2 = 1;
+   int num3 = 0;
    for (int i = 2; i <= n; i++)
    {
-         num3 = num1 + num2;
-         num1 = num2;
-         num2 = num3;
-         return num1;
-      
+      num3 = num1 + num2;
+      num1 = num2;
+      num2 = num3;
    }
    return num3;
 }
 
-int main(int __argc, char *__argv[])
+int main(int argc, char *argv[])
 {
+   
+   int i = 0;
    FILE *filePointer;
    int fileNum = 0;
    int nthNum = 0;
-   long long fibbonachiNum = 0;
+   long long int fibbonachiNum = 0;
    int num1 = 0;
 
-   if (__argc == 4)
+   if (argc == 4)
    {
-
-      filePointer = fopen(__argv[3], "r");
+      
+      filePointer = fopen(argv[3], "r");
       fscanf(filePointer, "%d", &fileNum);
       fclose(filePointer);
 
-      num1 = atoi(__argv[1]);
+      num1 = atoi(argv[1]);
       nthNum = num1 + fileNum;
 
-      if (*__argv[2] == 'i')
-      {
-         fibbonachiNum = fibbonachiItera(nthNum - 1);
-      }
-      else if (*__argv[2] == 'r')
-      {
-         fibbonachiNum = fibbonachiRecurs(nthNum - 1);
-      }
-   }
+    long long int *fibArray = calloc(nthNum,sizeof(long long int));
 
+      
+
+         if (*argv[2] == 'i')
+         {
+            fibbonachiNum = fibbonachiItera(nthNum - 1);
+         }
+         else if (*argv[2] == 'r')
+         {
+            fibbonachiNum = fibbonachiRecurs(nthNum - 1, fibArray);
+         }
+      
+      free(fibArray);
+   }
+   
    printf("%lld", fibbonachiNum);
 
    return 0;
