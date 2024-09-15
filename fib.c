318ef95581b107ae92f37e3107fc78c8e98760aa
smminassian <sebastienminassian203@gmail.com>
2024-09-14 19:53:27 -0700

    I kind of started working on memoization first. Now Im gonna save that code for later and just do the big  numbers one

--- fib.c
+++ fib.c
@@ -1,21 +1,91 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// int fibbonachiRecurs(int num, long long int *fibArray)
+// {
+//    long long int fibValue = 0;
+//    if (num == 0 || num == 1)
+//    {
+//       return num;
+//    }
+//    else if (fibArray[num] == 0)
+//    {
+//       fibValue = fibbonachiRecurs(num - 1, fibArray) + fibbonachiRecurs(num - 2, fibArray);
+//       fibArray[num] = fibValue;
+//    }
+
+//    return fibArray[num];
+// }
+
+// int fibbonachiItera(int n)
+// {
+//    int num1 = 0;
+//    int num2 = 1;
+//    int num3 = 0;
+//    for (int i = 2; i <= n; i++)
+//    {
+//       num3 = num1 + num2;
+//       num1 = num2;
+//       num2 = num3;
+//    }
+//    return num3;
+// }
+
+// int main(int __argc, char *__argv[])
+// {
+
+//    if (__argc == 4)
+//    {
+//       int i = 0;
+//       FILE *filePointer;
+//       int fileNum = 0;
+//       int nthNum = 0;
+//       long long int fibbonachiNum = 0;
+//       int num1 = 0;
+
+//       filePointer = fopen(__argv[3], "r");
+//       fscanf(filePointer, "%d", &fileNum);
+//       fclose(filePointer);
+
+//       num1 = atoi(__argv[1]);
+//       nthNum = num1 + fileNum;
+
+//       long long int *fibArray = calloc(nthNum, sizeof(long long int));
+
+//       fibArray[0] = 0;
+//       fibArray[1] = 1;
+
+//       if (*__argv[2] == 'i')
+//       {
+//          fibbonachiNum = fibbonachiItera(nthNum - 1);
+//       }
+//       else if (*__argv[2] == 'r')
+//       {
+//          fibbonachiNum = fibbonachiRecurs(nthNum - 1, fibArray);
+//       }
+
+//       free(fibArray);
+//       printf("%lld", fibbonachiNum);
+//    }
+
+//    return 0;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int fibbonachiRecurs(int num, long long int *fibArray)
+int fibbonachiRecurs(int num)
 {
-   long long int fibValue = 0;
    if (num == 0 || num == 1)
    {
       return num;
    }
-   else if (fibArray[num] == 0)
+   else
    {
-      fibValue = fibbonachiRecurs(num - 1, fibArray) + fibbonachiRecurs(num - 2, fibArray);
-      fibArray[num] = fibValue;
+      return fibbonachiRecurs(num - 1) + fibbonachiRecurs(num - 2);
    }
-
-   return fibArray[num];
 }
 
 int fibbonachiItera(int n)
@@ -25,24 +95,30 @@ int fibbonachiItera(int n)
    int num3 = 0;
    for (int i = 2; i <= n; i++)
    {
-      num3 = num1 + num2;
-      num1 = num2;
-      num2 = num3;
+      if (n != 0)
+      {
+         num3 = num1 + num2;
+         num1 = num2;
+         num2 = num3;
+      }
+      else
+      {
+         return num1;
+      }
    }
    return num3;
 }
 
 int main(int __argc, char *__argv[])
 {
+   FILE *filePointer;
+   int fileNum = 0;
+   int nthNum = 0;
+   int fibbonachiNum = 0;
+   int num1 = 0;
 
    if (__argc == 4)
    {
-      int i = 0;
-      FILE *filePointer;
-      int fileNum = 0;
-      int nthNum = 0;
-      long long int fibbonachiNum = 0;
-      int num1 = 0;
 
       filePointer = fopen(__argv[3], "r");
       fscanf(filePointer, "%d", &fileNum);
@@ -51,23 +127,17 @@ int main(int __argc, char *__argv[])
       num1 = atoi(__argv[1]);
       nthNum = num1 + fileNum;
 
-      long long int *fibArray = calloc(nthNum, sizeof(long long int));
-
-      fibArray[0] = 0;
-      fibArray[1] = 1;
-
       if (*__argv[2] == 'i')
       {
          fibbonachiNum = fibbonachiItera(nthNum - 1);
       }
       else if (*__argv[2] == 'r')
       {
-         fibbonachiNum = fibbonachiRecurs(nthNum - 1, fibArray);
+         fibbonachiNum = fibbonachiRecurs(nthNum - 1);
       }
-
-      free(fibArray);
-      printf("%lld", fibbonachiNum);
    }
+
+   printf("%d", fibbonachiNum);
 
    return 0;
 }
